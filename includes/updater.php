144dfcfd0ced81fe8fa6170d71eb9772f26a783a
Ben Plum <mr@benplum.com>
2023-04-05 15:50:23 -0400

    Initial commit

---
+++ includes/updater.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'vendor/pw-updater.php';
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+
+class Media_Sync_Updater extends PW_GitHub_Updater {
+
+  public $username = 'benplum';
+  public $repository = 'Media Sync';
+  public $requires = '5.0';
+  public $tested = '5.0.2';
+
+  public function __construct() {
+    $this->parent = Media_Sync::get_instance();
+
+    parent::__construct();
+  }
+
+}
+
+
+// Instance
+
+Media_Sync_Updater::get_instance();
